3416394cc458400da2e3409c3e9f4145e6068eb2
huang.dan.dan <huang.dan.dan@chowsangsang.com>
2026-06-05 13:41:33 +0800

    完善icon，引入material-icons

--- packages/components/icon/src/icon.vue
+++ packages/components/icon/src/icon.vue
@@ -1,24 +1,24 @@
 <template>
-  <svg
+  <component
+    :is="isFontIcon ? 'span' : 'svg'"
     :class="iconClasses"
-    :width="size"
-    :height="size"
-    :viewBox="viewBox"
     :style="iconStyle"
     :tabindex="tabindex"
     :aria-label="ariaLabel"
     :aria-hidden="ariaHidden"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
+    v-bind="isFontIcon ? {} : svgAttributes"
   >
-    <slot />
-  </svg>
+    <slot v-if="!isFontIcon" />
+    <template v-else>{{ name }}</template>
+  </component>
 </template>
 
 <script setup lang="ts">
 import { computed } from 'vue'
 
 interface IconProps {
+  name?: string
+  variant?: 'filled' | 'outlined' | 'round' | 'sharp' | 'two-tone'
   size?: number | string
   color?: string
   viewBox?: string
@@ -30,6 +30,8 @@ interface IconProps {
 }
 
 const props = withDefaults(defineProps<IconProps>(), {
+  name: '',
+  variant: 'filled',
   size: 24,
   color: 'currentColor',
   viewBox: '0 0 24 24',
@@ -39,6 +41,16 @@ const props = withDefaults(defineProps<IconProps>(), {
   ariaLabel: '',
   tabindex: undefined
 })
+
+const isFontIcon = computed(() => !!props.name)
+
+const svgAttributes = computed(() => ({
+  width: props.size,
+  height: props.size,
+  viewBox: props.viewBox,
+  fill: 'none',
+  xmlns: 'http://www.w3.org/2000/svg'
+}))
 
 const ariaHidden = computed(() => !props.ariaLabel)
 
@@ -53,37 +65,58 @@ const getSizeClass = (size: number | string): string | undefined => {
   return sizeMap[Number(size)]
 }
 
+const getMaterialIconClass = (variant: string): string => {
+  const variantMap: Record<string, string> = {
+    filled: 'material-icons',
+    outlined: 'material-icons-outlined',
+    round: 'material-icons-round',
+    sharp: 'material-icons-sharp',
+    'two-tone': 'material-icons-two-tone'
+  }
+  return variantMap[variant] || 'material-icons'
+}
+
 const iconClasses = computed(() => {
   const sizeClass = getSizeClass(props.size)
-  return [
+  const baseClasses = [
     'hdd-icon',
     sizeClass ? `hdd-icon--${sizeClass}` : '',
     {
       'hdd-icon--spin': props.spin
     }
   ]
+  
+  if (isFontIcon.value) {
+    baseClasses.push(getMaterialIconClass(props.variant))
+  }
+  
+  return baseClasses
 })
 
 const iconStyle = computed(() => {
-  const style: Record<string, string> = {}
+  const style: Record<string, string | number> = {}
   
   if (props.color && props.color !== 'currentColor') {
     style.color = props.color
   }
   
-  if (props.rotate !== 0) {
-    style.transform = `rotate(${props.rotate}deg)`
-  }
-  
-  if (props.flip) {
-    let flipTransform = ''
-    if (props.flip === 'horizontal' || props.flip === 'both') {
-      flipTransform += ' scaleX(-1)'
+  if (isFontIcon.value) {
+    style.fontSize = typeof props.size === 'number' ? `${props.size}px` : props.size
+  } else {
+    if (props.rotate !== 0) {
+      style.transform = `rotate(${props.rotate}deg)`
     }
-    if (props.flip === 'vertical' || props.flip === 'both') {
-      flipTransform += ' scaleY(-1)'
+    
+    if (props.flip) {
+      let flipTransform = ''
+      if (props.flip === 'horizontal' || props.flip === 'both') {
+        flipTransform += ' scaleX(-1)'
+      }
+      if (props.flip === 'vertical' || props.flip === 'both') {
+        flipTransform += ' scaleY(-1)'
+      }
+      style.transform = (style.transform || '') + flipTransform
     }
-    style.transform = (style.transform || '') + flipTransform
   }
   
   return style
